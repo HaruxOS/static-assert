3f6526545080c8bc81fe0b8368dee22516ffb15d
HTG-YT <htgazurex1212.0001@gmail.com>
2021-08-17 22:10:53 +0800

    init crate

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+ * File: src/lib.rs
+ *
+ * Static assertions for Rust.
+ *
+ * Author: HTG-YT
+ * Copyright (c) 2021 The HaruxOS Project Team
+ */
+
+#![no_std]
+
+#![allow(incomplete_features)]
+
+#![feature(const_panic)]
+#![feature(decl_macro)]
+#![feature(inline_const)]
+
+/// Statically asserts that an expression is true.
+pub macro static_assert {
+    ($($arguments:tt)*) => {
+        const _: () = const { core::assert!($($arguments)*) };
+    }
+}
+
+/// Statically asserts that two values are equal.
+pub macro static_assert_eq {
+    ($($arguments:tt)*) => {
+        const _: () = const { core::assert_eq!($($arguments)*) };
+    }
+}
+
+/// Statically asserts that two values are not equal.
+pub macro static_assert_ne {
+    ($($arguments:tt)*) => {
+        const _: () = const { core::assert_ne!($($arguments)*) };
+    }
+}
